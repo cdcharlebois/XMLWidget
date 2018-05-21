bcf61431fb61e92f27d0732f2e823dd209e7ca5e
Conner Charlebois <cdcharlebois@gmail.com>
2018-05-21 16:29:49 -0400

    editor onchange, swap the ace build to the define one, and set the config base path

--- src/XMLWidget/widget/XMLWidget.js
+++ src/XMLWidget/widget/XMLWidget.js
@@ -15,64 +15,69 @@ define([
     "dojo/html",
     "dojo/_base/event",
     "XMLWidget/lib/jquery-1.11.2",
-    "XMLWidget/lib/ace",
+    "XMLWidget/lib/ace2",
     "dojo/text!XMLWidget/widget/template/XMLWidget.html"
-], function (declare, _WidgetBase, _TemplatedMixin, dom, dojoDom, dojoProp, dojoGeometry, dojoClass, dojoStyle, dojoConstruct, dojoArray, lang, dojoText, dojoHtml, dojoEvent, _jQuery, _ace,widgetTemplate) {
+], function(declare, _WidgetBase, _TemplatedMixin, dom, dojoDom, dojoProp, dojoGeometry, dojoClass, dojoStyle, dojoConstruct, dojoArray, lang, dojoText, dojoHtml, dojoEvent, _jQuery, _ace, widgetTemplate) {
     "use strict";
 
     var $ = _jQuery.noConflict(true);
 
-    return declare("XMLWidget.widget.XMLWidget", [ _WidgetBase, _TemplatedMixin ], {
+    return declare("XMLWidget.widget.XMLWidget", [_WidgetBase, _TemplatedMixin], {
 
         templateString: widgetTemplate,
 
-
         widgetBase: null,
         //editor: null,
-        field:null,
-        node:null,
+        field: null,
+        node: null,
         onClickMicroflow: null,
         // Internal variables.
         _handles: null,
         _contextObj: null,
 
-        constructor: function () {
+        constructor: function() {
             this._handles = [];
         },
 
-        postCreate: function () {
+        postCreate: function() {
             logger.debug(this.id + ".postCreate");
-            
+            ace.config.set('basePath', '/widgets/XMLWidget/lib/');
+
         },
 
-        update: function (obj, callback) {
+        update: function(obj, callback) {
             logger.debug(this.id + ".update");
-            
+
             this._contextObj = obj;
             this._updateRendering(callback);
         },
 
-        resize: function (box) {
+        resize: function(box) {
             logger.debug(this.id + ".resize");
         },
 
-        uninitialize: function () {
+        uninitialize: function() {
             logger.debug(this.id + ".uninitialize");
         },
 
-        _updateRendering: function (callback) {
+        _updateRendering: function(callback) {
             logger.debug(this.id + "._updateRendering");
             var value = "" + this._contextObj.get(this.field);
             var editor = ace.edit("editor");
-            //editor.setTheme("ace/theme/monokai");
-            //editor.getSession().setMode("ace/mode/xml");
+            editor.getSession().setUseWorker(false);
+            // add the listener to update the context object when the editor's value changes
+            editor.on("change", function(data) {
+                this._contextObj.set(this.field, editor.getValue());
+            }.bind(this));
+
+            editor.setTheme("ace/theme/monokai");
+            editor.getSession().setMode("ace/mode/xml");
             editor.setValue(value);
             this._executeCallback(callback, "_updateRendering");
         },
-       
 
         // Shorthand for executing a callback, adds logging to your inspector
-        _executeCallback: function (cb, from) {
+        _executeCallback: function(cb, from) {
             logger.debug(this.id + "._executeCallback" + (from ? " from " + from : ""));
             if (cb && typeof cb === "function") {
                 cb();
